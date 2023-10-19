4a71c858bea9778f701909bb20fc02ad79926f3d
StartOff123 <startoff123@mail.ru>
2023-10-19 19:05:03 +0500

    cove-calendar2.0

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,31 @@
 import React from 'react'
 import { UserContext } from '../../App'
+import { Dropdown } from 'antd'
+import type { MenuProps } from 'antd'
+import { LogoutOutlined } from '@ant-design/icons'
 
 import '../../styles/header.scss'
 
 const Header = (): React.ReactElement => {
   const user = React.useContext(UserContext)
+
+  const handleLogout = () => {
+    window.localStorage.removeItem('access_token')
+    window.location.reload()
+  }
+
+  const items: MenuProps['items'] = [
+    {
+      key: '1',
+      label: (
+        <div onClick={handleLogout}>
+          <p>Выйти</p>
+        </div>
+      ),
+      icon: <LogoutOutlined />,
+      danger: true
+    },
+  ]
 
   return (
     <div className='header'>
@@ -12,15 +33,23 @@ const Header = (): React.ReactElement => {
         <img src="/assets/icon/heartHeader.png" alt="" />
         <h1>Love Calendar</h1>
       </div>
-      <div className='header__profile'>
-        <p>{user?.name}</p>
-        <div className='header__profile--img'>
-          <img
-            src={user?.AvatarUrl ? `${process.env.REACT_APP_SERVER_URL}/uploads/${user?.AvatarUrl}` : '/assets/static/no-avatar.png'}
-            alt="Avatar"
-          />
+      <Dropdown
+        trigger={['click']}
+        menu={{
+          items: items
+        }}
+      >
+        <div className='header__profile'>
+
+          <p>{user?.name}</p>
+          <div className='header__profile--img'>
+            <img
+              src={user?.AvatarUrl ? `${process.env.REACT_APP_SERVER_URL}/uploads/${user?.AvatarUrl}` : '/assets/static/no-avatar.png'}
+              alt="Avatar"
+            />
+          </div>
         </div>
-      </div>
+      </Dropdown>
     </div>
   )
 }
